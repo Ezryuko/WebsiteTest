b4b0c1c5fcff92df6f6a884bc8e90fb90a7248d7
Ezryuko <ezryuko@gmail.com>
2020-04-20 16:07:36 +0200

    u

--- js/mainFunctions.js
+++ js/mainFunctions.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
+  loadPage();
+});
+
+function loadPage(){
+  console.log("Loading navbar");
   $('.nav_container').load('.common/navbar.html');
+  console.log("navbar loaded");
+  console.log("Loading footer");
   $('.footer_container').load('.common/footer.html');
-})
+  console.log("Footer loaded");
+}
